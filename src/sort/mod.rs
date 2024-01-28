94078e70781331a1c137969e012c068f45987022
useEffects <joel.sr1024@gmail.com>
2024-01-28 23:09:00 +0530

    add insertion sort

--- src/sort/mod.rs
+++ src/sort/mod.rs
@@ -29,9 +29,21 @@ pub fn selection(arr: &mut Vec<i32>) {
     }
 }
 
+pub fn insertion(arr: &mut Vec<i32>) {
+    for i in 1..arr.len() {
+        let key = arr[i];
+        let mut j = i as isize - 1;
+        while j >= 0 && arr[j as usize] > key {
+            arr[j as usize + 1] = arr[j as usize];
+            j -= 1;
+        }
+        arr[(j + 1) as usize] = key;
+    }
+}
+
 #[test]
 fn test() {
-    let functions: Vec<fn(&mut Vec<i32>)> = vec![bubble, selection];
+    let functions: Vec<fn(&mut Vec<i32>)> = vec![bubble, selection, insertion];
     for func in functions {
         let mut unsorted_vector = vec![5, 2, 9, 1, 5, 6];
         let expected_result = vec![1, 2, 5, 5, 6, 9];
