98fd2890d4c0e807298c278facf07453d7a63cc8
useEffects <joel.sr1024@gmail.com>
2024-01-28 22:21:43 +0530

    added selection sort

--- src/sort/mod.rs
+++ src/sort/mod.rs
@@ -14,9 +14,24 @@ pub fn bubble(arr: &mut Vec<i32>) {
     }
 }
 
+pub fn selection(arr: &mut Vec<i32>) {
+    let mut _small = 0;
+    for i in 0..arr.len() {
+        _small = i;
+        for j in i + 1..arr.len() {
+            if arr[j] < arr[_small] {
+                _small = j;
+            }
+        }
+        if _small != i {
+            arr.swap(_small, i);
+        }
+    }
+}
+
 #[test]
 fn test() {
-    let functions: Vec<fn(&mut Vec<i32>)> = vec![bubble];
+    let functions: Vec<fn(&mut Vec<i32>)> = vec![bubble, selection];
     for func in functions {
         let mut unsorted_vector = vec![5, 2, 9, 1, 5, 6];
         let expected_result = vec![1, 2, 5, 5, 6, 9];
